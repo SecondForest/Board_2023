c71c0a289076cf3a2890263c5730c468b27fc8ab
Hyeongryeol.An <reversiblehope@gmail.com>
2023-11-27 10:51:54 +0900

    build.gradle 수정 서버 확인

---
+++ src/main/java/com/devsecond/board/MainController.java
@@ -0,0 +1,14 @@
+package com.devsecond.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@GetMapping("/board")
+	@ResponseBody
+	public String index() {
+		return "Hello Board";
+	}
+}

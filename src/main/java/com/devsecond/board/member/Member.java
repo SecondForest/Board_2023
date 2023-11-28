571f9664035273409b15615b7e63a09e3726623e
Hyeongryeol.An <reversiblehope@gmail.com>
2023-11-28 17:43:42 +0900

    Member 뼈대 회원가입 + SpringSecurity 설정

---
+++ src/main/java/com/devsecond/board/member/Member.java
@@ -0,0 +1,24 @@
+package com.devsecond.board.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String name;
+
+	private String password;
+
+	@Column(unique = true)
+	private String email;
+}
